2296966c420e18e7cf823c20e647f99cc92ed767
Naoyuki Yamada <n.yamada@cyberagentamerica.com>
2013-07-27 16:52:43 +0900

    アイコンのマッピングと、「使途別予算額」のページ表示

--- js/setting.js
+++ js/setting.js
@@ -16,26 +16,43 @@ OpenSpending.year = '2013';
 OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
-    '1': { icon: 'icons/helping-others.svg', color: '#128b56', bcolor: '#935B3B' },
-    '2': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '3': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '4': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '5': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '6': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '7': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '8': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '9': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '10': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '11': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '12': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '13': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '14': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '15': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '16': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '17': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '18': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '19': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-    '20': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' }
+    '1': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#935B3B' }, //議会
+    '2': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#935B3B' }, //総務
+    '3': { icon: 'icons/planning.svg', color: '#C75746', bcolor: '#935B3B' }, //民生 / 徴税
+    '4': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#935B3B' }, //衛生 / 戸籍住民基本台帳
+    '5': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#935B3B' }, //労務 / 選挙
+    '6': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#935B3B' }, //農業 / 統計調査
+    '7': { icon: 'icons/human-resources.svg', color: '#C75746', bcolor: '#935B3B' }, //商工 / 監視委員
+    '8': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#935B3B' }, //土木 / 社会福祉
+    '9': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#935B3B' }, //消防 / 老人福祉
+    '10': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#935B3B' }, //教育 / 児童福祉
+    '11': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#935B3B' }, //公債 / 生活保護
+    '12': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#935B3B' }, //諸支出 / 災害救助
+    '13': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#935B3B' }, //予備 / 保健衛生
+    '14': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#935B3B' }, //清掃
+    '15': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#935B3B' }, //環境保全
+    '16': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#935B3B' }, //労働諸費
+    '17': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#935B3B' }, //農業
+    '18': { icon: 'icons/human-resources.svg', color: '#C75746', bcolor: '#935B3B' }, //商工
+    '19': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#935B3B' }, //土木管理
+    '20': { icon: 'icons/transport.svg', color: '#C75746', bcolor: '#935B3B' }, //道路橋りょう
+    '21': { icon: 'icons/water.svg', color: '#C75746', bcolor: '#935B3B' }, //河川
+    '22': { icon: 'icons/transport.svg', color: '#C75746', bcolor: '#935B3B' }, //都市計画
+    '23': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#935B3B' }, //住宅
+    '24': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#935B3B' }, //消防
+    '25': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#935B3B' }, //教育総務
+    '26': { icon: 'icons/primary.svg', color: '#C75746', bcolor: '#935B3B' }, //小学校
+    '27': { icon: 'icons/secondary-lower.svg', color: '#C75746', bcolor: '#935B3B' }, //中学校
+    '28': { icon: 'icons/secondary-upper.svg', color: '#C75746', bcolor: '#935B3B' }, //高等学校
+    '29': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#935B3B' }, //幼稚園
+    '30': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#935B3B' }, //社会教育
+    '31': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#935B3B' }, //学校保健
+    '32': { icon: 'icons/sports.svg', color: '#C75746', bcolor: '#935B3B' }, //体育
+    '33': { icon: 'icons/family2.svg', color: '#C75746', bcolor: '#935B3B' }, //教育諸費
+    '34': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#935B3B' }, //公債
+    '35': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' }, //公営企業
+    '36': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#935B3B' }, //地方公共団体金融機構納付
+    '37': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#935B3B' } //予備
 };
 
 /*
