7f0ba8967f844351743afd3a2ff640389268c6f6
Naoyuki Yamada <n.yamada@cyberagentamerica.com>
2013-07-27 15:44:47 +0900

    データの置き換え

--- js/setting.js
+++ js/setting.js
@@ -16,6 +16,33 @@ OpenSpending.year = '2013';
 OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '10-25': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' }
+    '1': { icon: 'icons/helping-others.svg', color: '#128b56', bcolor: '#935B3B' },
+    '2': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '3': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '4': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '5': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '6': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '7': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '8': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '9': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '10': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '11': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '12': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '13': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '14': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '15': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '16': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '17': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '18': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '19': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+    '20': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' }
 };
+
+/*
+ #128b56
+
+ #C75746
+
+ #935B3B
+
+ */
